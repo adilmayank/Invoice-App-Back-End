3d09d1e7cbdc0a4304694f2e7c5a5f57effdd1b0
adilmayank <adilmayank5894@gmail.com>
2023-02-21 16:02:02 +0530

    added front end address as origin

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:3000',
-      'https://invoice-app-backend-gkni.onrender.com',
+      'https://adil-invoice-app.netlify.app/',
     ],
   })
 )
