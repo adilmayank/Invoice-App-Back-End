bf58e4f58f773416238c3e849d5fde496d5097e4
adilmayank <adilmayank5894@gmail.com>
2023-03-16 18:16:04 +0530

    Added Projection to database query or result to client

--- Controllers/ProductController.js
+++ Controllers/ProductController.js
@@ -1,43 +1,64 @@
 const { ProductModel } = require('../Models')
 
 exports.getAllProducts = (req, res) => {
-  ProductModel.find({})
+  const returningProperties = '_id stockCode name'
+  ProductModel.find({}, returningProperties)
     .then((result) => {
-      res.status(200).json({ data: result })
+      if (!result) {
+        res
+          .status(200)
+          .json({ data: null, msg: 'no records found', count: null })
+      } else {
+        res
+          .status(200)
+          .json({ data: result, msg: 'success', count: result.length })
+      }
     })
     .catch((err) => {
-      res.status(500).json({ error: err })
+      res.status(500).json({ error: err, msg: 'error' })
     })
 }
 
 exports.getSingleProduct = (req, res) => {
   const { productId } = req.params
-  ProductModel.findById(productId)
+  const returningProperties =
+    '_id stockCode name description unitPrice hsnNumber'
+  ProductModel.findById(productId, returningProperties)
     .then((result) => {
-      res.status(200).json({ data: result })
+      if (!result) {
+        res.status(200).json({ data: null, msg: 'no record found' })
+      } else {
+        res.status(200).json({ data: result, msg: 'success' })
+      }
     })
     .catch((err) => {
-      res.status(500).json({ error: err })
+      res.status(500).json({ error: err, msg: 'error' })
     })
 }
 
 exports.updateSingleProduct = (req, res) => {
-  const { productId, data } = req.body
-  for (key of Object.keys(data)) {
-    if (data[key] === undefined || data[key] === null) {
-      delete data[key]
-    }
-  }
+  const {
+    productId,
+    data: { name, stockCode, description, unitPrice, hsnNumber },
+  } = req.body
+
+  // creating custom data object to feed to update
+  const data = { ...name, stockCode, description, unitPrice, hsnNumber }
   ProductModel.findByIdAndUpdate(
     productId,
     { ...data, modifiedOn: Date.now() },
     { runValidators: true, new: true }
   )
     .then((result) => {
-      res.status(201).json({ data: result })
+      if (!result) {
+        res.status(200).json({ data: null, msg: 'no record found' })
+      } else {
+        const { _id } = result
+        res.status(200).json({ data: { productId: _id }, msg: 'success' })
+      }
     })
     .catch((err) => {
-      res.status(500).json({ error: err })
+      res.status(500).json({ error: err, msg: 'error' })
     })
 }
 
@@ -46,10 +67,16 @@ exports.deleteSingleProduct = (req, res) => {
 
   ProductModel.findByIdAndRemove(productId)
     .then((result) => {
-      res.status(201).json({ data: result })
+      if (!result) {
+        res.status(200).json({ data: null, msg: 'no record found' })
+      } else {
+        const { name, stockCode } = result
+        res.status(200).json({ data: { name, stockCode }, msg: 'success' })
+      }
     })
     .catch((err) => {
-      res.status(500).json({ error: err })
+      console.log(err)
+      res.status(500).json({ error: err, msg: 'error' })
     })
 }
 
@@ -59,9 +86,10 @@ exports.createProduct = (req, res) => {
   newProduct
     .save()
     .then((result) => {
-      res.status(201).json({ data: result })
+      const { _id } = result
+      res.status(201).json({ data: { productId: _id }, msg: 'success' })
     })
     .catch((err) => {
-      res.status(500).json({ error: err })
+      res.status(500).json({ error: err, msg: 'error' })
     })
 }
