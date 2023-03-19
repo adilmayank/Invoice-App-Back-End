01d67c687921d38b4c9f3c0cbdb9e2dcc8363567
adilmayank <adilmayank5894@gmail.com>
2023-03-19 21:09:32 +0530

    Removed Aggregate middleware references

--- Middlewares/index.js
+++ Middlewares/index.js
@@ -1,11 +1,7 @@
-const { QuotationAggregate } = require('./QuotationAggregate')
-const { InvoiceAggregate } = require('./InvoiceAggregate')
 const {
   ApplicationLevelAuthentication,
 } = require('./ApplicationLevelAuthentication')
 
 module.exports = {
-  QuotationAggregate,
-  InvoiceAggregate,
   ApplicationLevelAuthentication,
 }
