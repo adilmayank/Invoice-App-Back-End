ca23fade009bb8d948e111391e9f8d0ed2477c98
adilmayank <adilmayank5894@gmail.com>
2023-03-19 21:07:26 +0530

    added index for utils

---
+++ utils/index.js
@@ -0,0 +1,9 @@
+const { InvoiceAggregate } = require('./InvoiceAggregate')
+const { QuotationAggregate } = require('./QuotationAggregate')
+const { UpdateInvoicePaymentStatus } = require('./UpdateInvoicePaymentStatus')
+
+module.exports = {
+  InvoiceAggregate,
+  QuotationAggregate,
+  UpdateInvoicePaymentStatus,
+}
